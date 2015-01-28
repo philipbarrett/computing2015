afc29aece9f9bfb669c1ca044d25d32f94c29262
Philip Barrett <pobarrett@gmail.com>
2015-01-28 11:33:38 -0600

    Changed to include snow.

--- parallel/clusterStart.R
+++ parallel/clusterStart.R
@@ -1,4 +1,6 @@
 # clusterStart.R: Script to start a cluster
 
-library(parallel)
+# library(parallel)
+library(snow)
+    # Using snow only to make charts
 cl <- makeCluster(mpi.universe.size(), type = "MPI")
